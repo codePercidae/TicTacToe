cea8992b0ce0afcc13fdc219dbec9f34ae2c0ec8
Tommi <kiiski.tommi@hotmail.com>
2021-04-13 16:49:00 +0300

    Create game.py

---
+++ game.py
@@ -0,0 +1,12 @@
+#building the game fied
+def game_field():
+    lst1 = [[] for i in range(3)]
+    lst2 = [[] for i in range(3)]
+    lst3 = [[] for i in range(3)]
+    field = (lst1, lst2, lst3)
+    for i in field:
+        print(i)
+
+game_field()
+
+#creating the logic
